1d4d31409fa2015d8af60402c65e92799e70af59
nonis3 <nonis@purdue.edu>
2017-08-17 18:36:30 -0400

    Cleaning up deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# Build books file
+ruby scripts/goodreads-scraper/build.rb > partials/books.html
+
+# Delete cache
 rm -rf _site
 ./site clean
+
+# Rebuild
 ./site build
+
+# Upload (commented out since I build on my server)
 #rsync -avz _site/ root@austinschwartz.com:/var/www/austinschwartz.com/
 
-sudo cp _site/* /var/www/austinschwartz.com/
+# Copy to our static site directory :)
+sudo cp -r _site/* /var/www/html/
+
